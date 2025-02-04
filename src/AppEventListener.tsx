31cc782a27a4e674cd134fee875c367f9bd5dd04
chandresh <chandresh.shukla@resilientitservices.com>
2025-02-04 20:10:41 +0530

    second

--- src/AppEventListener.tsx
+++ src/AppEventListener.tsx
@@ -1,8 +1,9 @@
-import { ReactElement, useState, useEffect, useRef ,createElement} from "react";
-import {View, AppState,ViewStyle,TextStyle} from "react-native";
+import {ReactElement, useState, useEffect, useRef, createElement} from "react";
+import {View, AppState, ViewStyle, TextStyle} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage"; // For storing timestamps
-import { AppEventListenerProps } from "../typings/AppEventListenerProps";
-import { Style ,mergeNativeStyles} from "@mendix/pluggable-widgets-tools";
+import {AppEventListenerProps} from "../typings/AppEventListenerProps";
+import {Style, mergeNativeStyles} from "@mendix/pluggable-widgets-tools";
+
 declare const mx: any; // Mendix global object
 export interface CustomStyle extends Style {
     container: ViewStyle;
@@ -12,17 +13,18 @@ export interface CustomStyle extends Style {
 const defaultStyle: CustomStyle = {
     container: {
         flex: 1,
-        width:"100%",
+        width: "100%",
         alignItems: "center",
         justifyContent: "center",
-        zIndex:1000000,
-        height:700
+        zIndex: 1000000,
+        height: 700
     },
-    label:{
+    label: {
         fontSize: 14,
-        color:"red"
+        color: "red"
     }
 }
+
 export function AppEventListener(props: AppEventListenerProps<CustomStyle>): ReactElement {
     // const { onExit, onResume } = props;
     const [appState, setAppState] = useState(AppState.currentState);
@@ -32,38 +34,16 @@ export function AppEventListener(props: AppEventListenerProps<CustomStyle>): Rea
     useEffect(() => {
         const onAppStateChange = async (nextAppState: any) => {
             console.error(`App State Change : ${appStateRef.current} → ${nextAppState}`);
-            console.error('AppState',appState)
+            console.error('AppState', appState)
             if (appStateRef.current.match(/inactive|background/) && nextAppState === "active") {
-                // App comes to foreground, check if more than 15 sec has passed
-                const lastBackgroundTime = await AsyncStorage.getItem("backgroundTimestamp");
-                console.error("Last Background Time",lastBackgroundTime)
-                if (lastBackgroundTime) {
-                    const timeDiff = (Date.now() - parseInt(lastBackgroundTime, 10)) / 1000; // Convert to seconds
-                    console.error(`App was in background for ${timeDiff} seconds`);
-                    console.error(`restart scheduled for anything more than ${props.delay} seconds`);
-
-                    if (timeDiff > props.delay) {
-                        console.error(`show message`);
-                        mx.ui.openForm("HotelNativeMobile/PG_Idle.page.xml", {
-                            location: "popup",
-                            callback: function(form) {
-                                console.error(`restarting app now`);
-                                setTimeout(() => {
-                                    mx.reload(); // Reload after 3 seconds
-                                }, 1500);
-                            }
-                        });
-
-                        // // let time = Platform.OS === "ios" ? props.reloadtimeIos : props.reloadtime
-                        // setTimeout(() => {
-                        //     mx.reload(); // Reload after 3 seconds
-                        // }, 1500);
-                    }
+                if (props.onResume && props.onResume.canExecute) {
+                    props.onResume.execute();
                 }
 
             } else if (appStateRef.current === "active" && nextAppState.match(/inactive|background/)) {
-                console.error("App is going to the background!");
-                await AsyncStorage.setItem("backgroundTimestamp", Date.now().toString()); // Store time
+                if (props.onExit && props.onExit.canExecute) {
+                    props.onExit.execute();
+                }
             }
 
             appStateRef.current = nextAppState;
